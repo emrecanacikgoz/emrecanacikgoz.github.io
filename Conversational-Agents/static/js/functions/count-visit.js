c146652bf7c2265b701f0c579bce10ec4d182edd
Emre Can Acikgoz <eacikgoz17@ku.edu.tr>
2024-11-07 13:10:25 -0600

    .

--- Conversational-Agents/static/js/functions/count-visit.js
+++ Conversational-Agents/static/js/functions/count-visit.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     const axios = require('axios');
   
     // Make an API call to increment counter
-    await axios.post('https://your-server.com/increment-counter', {
+    await axios.post('https://emrecanacikgoz.github.io/Conversational-Agents//increment-counter', {
       url: event.headers.referer,
     });
   
